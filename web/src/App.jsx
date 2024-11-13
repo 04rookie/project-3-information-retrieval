a6eb32908712c8ebedc23f772fbfaae06d2c91af
04rookie <atharva.ek@gmail.com>
2024-11-13 17:17:43 -0500

    init web

---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import ChatWindow from './ChatWindow/ChatWindow'
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <ChatWindow / >
+      </div>
+    </>
+  )
+}
+
+export default App
