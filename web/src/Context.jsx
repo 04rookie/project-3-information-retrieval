7956e4798b00f4f8c6c473918578916aef9ccf37
04rookie <atharva.ek@gmail.com>
2024-11-22 23:48:08 -0500

    Filter context

--- web/src/Context.jsx
+++ web/src/Context.jsx
@@ -5,12 +5,25 @@ const DataContext = createContext();
 
 const DataProvider = ({ children }) => {
   const [chat, setChat] = useState([]);
-
+  const [filter, setFilter] = useState({
+    health: false,
+    environment: false,
+    technology: false,
+    economy: false,
+    entertainment: false,
+    sports: false,
+    politics: false,
+    education: false,
+    travel: false,
+    food: false,
+  });
   return (
     <DataContext.Provider
       value={{
         chat,
         setChat,
+        filter,
+        setFilter,
       }}
     >
       {children}
